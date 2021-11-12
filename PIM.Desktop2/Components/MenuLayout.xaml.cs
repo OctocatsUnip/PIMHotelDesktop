00680acd2320a54c2cdb2067ad6ed7c6a6dd7c5e
Thayane Bomfim <thayane_bomfims@hotmail.com>
2021-11-11 22:51:09 -0300

    Adicionado PIM 2 com back-end de exemplo

---
+++ PIM.Desktop2/Components/MenuLayout.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PIM.Desktop.Components
+{
+    /// <summary>
+    /// Interaction logic for MenuLayout.xaml
+    /// </summary>
+    public partial class MenuLayout : UserControl
+    {
+        public MenuLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}

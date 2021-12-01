c05e519af6822147216ab47019c75d6e599f9674
Thayane Bomfim <thayane_bomfims@hotmail.com>
2021-12-01 20:44:05 -0300

    Listar quartos

--- PIM.Desktop/MVVM/Model/StatusModel.cs
+++ PIM.Desktop/MVVM/Model/StatusModel.cs
@@ -8,6 +8,8 @@ namespace PIM.Desktop.MVVM.Model
     class StatusModel
     {
         public int id { get; set; }
-        public string nome_status { get; set; }
+        public string nome_status { get; set; }      
+        
+        public QuartosModel quartos { get; set; }
     }
 }

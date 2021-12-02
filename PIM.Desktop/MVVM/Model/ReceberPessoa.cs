6392a487d7c2477ad350eb428b8137b69be512b5
nicolasadekz <nicolas@adekz.com>
2021-12-01 22:42:57 -0300

    Fim da tela cadastro funcionario

---
+++ PIM.Desktop/MVVM/Model/ReceberPessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace PIM.Desktop.MVVM.Model
+{
+    public class ReceberPessoa
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string rg { get; set; }
+        public string cpf { get; set; }
+        public string sexo { get; set; }
+        public string data_nascimento { get; set; }
+
+
+    }
+}
--- PIM.Desktop/MVVM/View/CadastroFuncionarios.xaml.cs
+++ PIM.Desktop/MVVM/View/CadastroFuncionarios.xaml.cs
@@ -44,39 +44,25 @@ namespace PIM.Desktop
 
         }
 
-        private void GetPessoas()
+        private int GetPessoas()
         {
             var response = client.GetStringAsync(Url).Result;
-            var pessoas = JsonConvert.DeserializeObject<List<CadastroFuncionariosModel>>(response);
 
-            Console.WriteLine(pessoas);
+            var pessoas = JsonConvert.DeserializeObject<List<ReceberPessoa>>(response);
+
+            var ultimoIndice = pessoas.Count;
+            
+            return pessoas[ultimoIndice - 1].id;
+
+
         }
 
         private void btnSalvar_Click(object sender, RoutedEventArgs e)
         {
-            DateTime? selectedDate = nascimento.SelectedDate;
 
-            if (selectedDate.HasValue)
-            {
-                formatted = selectedDate.Value.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-            }
-
-            CadastroFuncionariosModel funcionario = new CadastroFuncionariosModel()
-            {
-
-                nome = Convert.ToString(nome.Text),
-                rg = Convert.ToString(rg.Text),
-                cpf = Convert.ToString(cpf.Text),
-                data_nascimento = Convert.ToDateTime(formatted),
-                sexo = Convert.ToString(sexo.Text),
-
-            };
-            this.SaveCadastroFuncionario(funcionario);
-            this.GetPessoas();
-
-            //Funcionario telaFuncionario = new Funcionario();
-            //telaFuncionario.Show();
-            //this.Close();
+            Funcionario telaFuncionario = new Funcionario(this.GetPessoas());
+            telaFuncionario.Show();
+            this.Close();
 
         }
 

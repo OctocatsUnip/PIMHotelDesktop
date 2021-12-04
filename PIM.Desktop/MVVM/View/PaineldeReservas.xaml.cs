59700555020060f6f4eb0eb4042b495f84792986
MileneMaciel <89492025+MileneMaciel@users.noreply.github.com>
2021-12-04 18:47:11 -0300

    exclusão de tela

--- PIM.Desktop/MVVM/View/PaineldeReservas.xaml.cs
+++ PIM.Desktop/MVVM/View/PaineldeReservas.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using PIM.Desktop.MVVM.Model;
+
 
 namespace PIM.Desktop
 {
@@ -17,10 +20,25 @@ namespace PIM.Desktop
     /// Lógica interna para PaineldeReservas.xaml
     /// </summary>
     public partial class PaineldeReservas : Window
+
     {
+        private string Url = "http://localhost:5000/";
+        HttpClient client = new HttpClient();
+
+
         public PaineldeReservas()
-        {
+        { client.BaseAddress = new Uri(Url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
+                );
+
             InitializeComponent();
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }

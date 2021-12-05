988b5a5d842f5a5d78da71a0f3c00eda0eec8c4a
MileneMaciel <89492025+MileneMaciel@users.noreply.github.com>
2021-12-05 13:24:21 -0300

    Painel de lista Reservas

---
+++ PIM.Desktop/MVVM/View/HttpClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PIM.Desktop
+{
+    internal class HttpClient
+    {
+        public Uri BaseAddress { get; internal set; }
+        public object DefaultRequestHeaders { get; internal set; }
+
+        internal object GetStringAsync(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
